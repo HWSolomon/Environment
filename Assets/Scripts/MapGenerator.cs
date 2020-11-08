2c24a3e34c0199dd6ef40a5c883c22cc25664a0e
HBlueRainDrop <43329986+HBlueRainDrop@users.noreply.github.com>
2020-11-08 17:24:07 +0100

    mesh
    Mesh kinda works

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,85 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using UnityEngine;
+using System.Collections;
 
-public class MapGenerator : MonoBehaviour
-{
-    public enum DrawMode { NoiseMap, ColourMap, Mesh };
-    public DrawMode drawMode;
+public class MapGenerator : MonoBehaviour {
 
-    public int mapWidth;
-    public int mapHeight;
-    public float noiseScale;
+	public enum DrawMode {NoiseMap, ColourMap, Mesh};
+	public DrawMode drawMode;
 
-    public int octaves;
+	public int mapWidth;
+	public int mapHeight;
+	public float noiseScale;
 
-    public float persistance;
-    public float lacunarity;
+	public int octaves;
+	[Range(0,1)]
+	public float persistance;
+	public float lacunarity;
 
-    public int seed;
-    public Vector2 offset;
+	public int seed;
+	public Vector2 offset;
 
-    public bool autoUpdate;
+	public bool autoUpdate;
 
-    public TerrainType[] regions;
-    public void GenerateMap()
-    {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+	public TerrainType[] regions;
 
-        Color[] colourMap = new Color[mapWidth * mapHeight];
-        for (int y = 0; y < mapHeight; y++)
-        {
-            for (int x = 0; x < mapWidth; x++)
-            {
-                float currentHeight = noiseMap[x, y];
-                for (int i = 0; i < regions.Length; i++)
-                {
-                    if(currentHeight <= regions[i].height)
-                    {
-                        colourMap[y * mapWidth + x] = regions[i].colour;
-                        break;
-                    }
-                }
-            }
-        }
+	public void GenerateMap() {
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
-        MapDisplay display = FindObjectOfType<MapDisplay>();
-        if(drawMode == DrawMode.NoiseMap)
-        {
-            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
-        } else if(drawMode == DrawMode.ColourMap){
-            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
-        } else if(drawMode == DrawMode.Mesh){
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
-        }
-    }
+		Color[] colourMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions [i].height) {
+						colourMap [y * mapWidth + x] = regions [i].colour;
+						break;
+					}
+				}
+			}
+		}
 
-    void OnValidate()
-    {
-        if (mapWidth < 1)
-        {
-            mapWidth = 1;
-        }
-        if (mapHeight < 1)
-        {
-            mapHeight = 1;
-        }
-        if (lacunarity < 1)
-        {
-            lacunarity = 1;
-        }
-        if (octaves < 0)
-        {
-            octaves = 0;
-        }
-    }
+		MapDisplay display = FindObjectOfType<MapDisplay> ();
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture (TextureGenerator.TextureFromHeightMap (noiseMap));
+		} else if (drawMode == DrawMode.ColourMap) {
+			display.DrawTexture (TextureGenerator.TextureFromColourMap (colourMap, mapWidth, mapHeight));
+		} else if (drawMode == DrawMode.Mesh) {
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (noiseMap), TextureGenerator.TextureFromColourMap (colourMap, mapWidth, mapHeight));
+		}
+	}
+
+	void OnValidate() {
+		if (mapWidth < 1) {
+			mapWidth = 1;
+		}
+		if (mapHeight < 1) {
+			mapHeight = 1;
+		}
+		if (lacunarity < 1) {
+			lacunarity = 1;
+		}
+		if (octaves < 0) {
+			octaves = 0;
+		}
+	}
 }
 
 [System.Serializable]
-public struct TerrainType
-{
-    public string name;
-    public float height;
-    public Color colour;
+public struct TerrainType {
+	public string name;
+	public float height;
+	public Color colour;
 }
